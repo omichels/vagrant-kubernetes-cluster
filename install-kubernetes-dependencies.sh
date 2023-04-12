9e981c402ade5b64da9e6a3f5815d3adcca125fb
Oliver Michels <oliver.michels@gmail.com>
2023-04-12 13:19:14 +0200

    k8s v1.25.8

--- install-kubernetes-dependencies.sh
+++ install-kubernetes-dependencies.sh
@@ -8,7 +8,7 @@ sudo apt -y install curl apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update
-sudo apt -y install vim git curl wget kubelet=1.24.12-00 kubeadm=1.24.12-00 kubectl=1.24.12-00
+sudo apt -y install vim git curl wget kubelet=1.25.8-00 kubeadm=1.25.8-00 kubectl=1.25.8-00
 sudo apt-mark hold kubelet kubeadm kubectl
 }
 
